47979563e948ab217a3450436ba0cc429005b6ad
Prisfenoy <prisci230604@gmail.com>
2026-06-10 14:12:37 -0300

    Agrego comentarios al archivo (#2)

--- scripts/grafico.py
+++ scripts/grafico.py
@@ -1,14 +1,17 @@
+# Librerias
 import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
-# Hola hola 
+
+# Cargamos los datos
 datos = pd.read_csv("titanic.csv")
-# Hola hola 
+
+# Grafico 1
 fig, ax = plt.subplots()
 sns.countplot(x = "Sex", hue = "Survived", data = datos, ax = ax)
-fig.savefig("plot.png")
-# Hola hola 
-# Hola hola 
+fig.savefig("supervivencia_por_sexo.png")
+
+# Grafico 2
 fig, ax = plt.subplots()
-sns.countplot(x = "Sex", hue = "Pclass", data = datos, ax = ax)
-fig.savefig("plot2.png")
+sns.countplot(x = "Pclass", hue = "Survived", data = datos, ax = ax)
+fig.savefig("supervivencia_por_clase.png")
